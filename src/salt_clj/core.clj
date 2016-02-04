a644cfe5fea00f001a425912b21d8312b34c22e4
Frantisek Kobzik <fkobzik@suse.de>
2016-02-04 11:18:13 +0100

    Added doc strings

--- src/salt_clj/core.clj
+++ src/salt_clj/core.clj
@@ -4,7 +4,12 @@
             [clojure.walk :as walk]
             [salt-clj.util :as util]))
 
-(defn login! [url params]
+(defn login!
+  "Log in. Example params:
+    {:username \"root\" 
+     :password \"root\"
+     :eauth \"auto\"}"
+  [url params]
   (let [salt-token (-> 
                      (http/post (str url "/login") {:form-params params})
                      (util/parse-happy-response)
@@ -13,25 +18,48 @@
      :password salt-token}))
 
 ; todo check - it doesn't work (although the API returns 200!!!!!)
-(defn logout! [token]
+(defn logout!
+  "Log out."
+  [token]
   (util/salt-post token "logout" nil))
 
 (defn minions 
+  "List minion(s). Optional param: minion id"
   ([token]
    (minions token nil))
   ([token mid]
    (util/salt-get token "minions" (util/nil-safe-name mid))))
 
-(defn minions! [token data]
+(defn minions!
+  "Run a job based on given data. Example data (running command):
+     {:username \"root\"
+      :password \"root\"
+      :eauth \"auto\"
+      :fun \"cmd.run\"
+      :arg \"sleep 3 && date\"
+      :tgt \"*\"
+      :client \"local\"}\"}
+  Example data (pinging minions):  
+     {:username \"root\"
+      :password \"root\"
+      :eauth \"auto\"
+      :fun \"test.ping\"
+      :tgt \"*\"
+      :client \"local\"}\"."
+  [token data]
   (util/salt-post token "minions" data))
   
 (defn jobs
+  "List job(s)."
   ([token] 
    (jobs token nil))
   ([token jid]
    (util/salt-get token "jobs" (util/nil-safe-name jid))))
 
-(defn run! [token data]
+(defn run!
+  "Synchronously run a job under given credentials (doesn't require the login
+  token) based on given data. See doc for minions! for examples."
+  [token data]
   (util/salt-post token "run" data))
 
 ; todo test (404 in my setup also in the java lib)
